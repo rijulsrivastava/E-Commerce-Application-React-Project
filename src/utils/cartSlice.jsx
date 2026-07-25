a833e883b6b8f51aca8c02782712902350b60132
rijulsrivastava <rijulsrivastva@gmail.com>
2026-07-25 16:54:42 +0530

    cartSlice is created using createSlice

---
+++ src/utils/cartSlice.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { createSlice } from '@reduxjs/toolkit'
+
+export const cartSlice = createSlice({
+    name: 'cart',
+    initialState: {
+        items: []
+    },
+    reducers: {
+        addItem: (state) => {
+            state.items.push()
+        }
+    }
+})
+
+export const { addItem } = cartSlice.actions
+export default cartSlice.reducer
