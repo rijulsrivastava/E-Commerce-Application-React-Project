73996822d953039a8f068c975b0999d6816e5e02
rijulsrivastava <rijulsrivastva@gmail.com>
2026-07-26 00:09:45 +0530

    Product filtering based on search functionality has been added

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ProductItem from './ProductItem'
+import { useSelector } from 'react-redux'
 
 function ProductList({ allProducts }) {
+
+  const searchText = useSelector((store) => store.search.searchText)
+  const filteredProducts = searchText.trim() == "" ? allProducts : allProducts.filter((product) => {
+    return (
+
+      product.brand && product.brand.toLowerCase().includes(searchText.toLowerCase()) ||
+      product.title.toLowerCase().includes(searchText.toLowerCase())
+    )
+  })
+
+  if (filteredProducts.length < 1) {
+    return <div className='flex justify-center items-center font-bold text-2xl m-10'>
+      <h2>No Matching Product</h2>
+    </div>
+  }
+
   return (
     <div className='flex flex-wrap justify-center items-center gap-6 py-4'>
-      {allProducts.map((product) => {
+      {filteredProducts.map((product) => {
         return (
           <ProductItem product={product} />
         )
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { setSearchText } from '../utils/searchSlice'
 
@@ -7,15 +7,14 @@ function Search() {
     const dispatch = useDispatch()
     const [input, setInput] = useState('')
 
-    function handleSearch() {
-        dispatch(setSearchText(input))
-    }
+    useEffect(() => {
+        dispatch(setSearchText(input.trim()))
+    }, [input, dispatch])
 
     return (
         <div>
             <div className='flex gap-2 items-center justify-center m-10'>
-                <input type="text" placeholder='Search for products' onChange={(e) => setInput(e.target.value)} className='border w-[500px] text-center p-1 px-2' />
-                <button className='border p-2 py-1' onClick={handleSearch}>Search</button>
+                <input type="text" value={input} placeholder='Search for products' onChange={(e) => setInput(e.target.value)} className='border w-[500px] text-center p-1 px-2' />
             </div>
 
         </div>
